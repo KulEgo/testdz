216b8c9626d7cf0a52c9eba9aa790ac318da02da
KulEgo <guitarmeeeeeeen@gmail.com>
2025-06-14 19:11:18 +0300

    Update locustfile.py

--- tests/locust/locustfile.py
+++ tests/locust/locustfile.py
@@ -4,9 +4,12 @@ class ShortistUser(HttpUser):
     wait_time = between(1, 3)
 
     @task
-    def shorten(self):
-        response = self.client.post("/api/shorten", json={"target_url": "https://example.com"})
-        if response.status_code == 200:
-            short_code = response.json().get("short_code")
-            if short_code:
-                self.client.get(f"/{short_code}", catch_response=True)
+    def create_short_link(self):
+        url = "https://example.com"
+        self.client.post("/shorten", json={"url": url})
+
+    @task
+    def redirect_short_link(self):
+        # Можно использовать заранее известный short_code или создать новый
+        short_code = "abc123"
+        self.client.get(f"/{short_code}")
