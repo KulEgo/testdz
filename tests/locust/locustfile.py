66c5a3e0463d6c877c5b90573efb98e17c0378b8
KulEgo <guitarmeeeeeeen@gmail.com>
2025-06-14 19:26:13 +0300

    Update locustfile.py

--- tests/locust/locustfile.py
+++ tests/locust/locustfile.py
@@ -3,13 +3,11 @@ from locust import HttpUser, task, between
 class ShortistUser(HttpUser):
     wait_time = between(1, 3)
 
-    @task
-    def create_short_link(self):
-        url = "https://example.com"
-        self.client.post("/shorten", json={"url": url})
+    @task(3)
+    def shorten_link(self):
+        self.client.post("/shorten", json={"url": "https://example.com"})
 
-    @task
-    def redirect_short_link(self):
-        # Можно использовать заранее известный short_code или создать новый
-        short_code = "abc123"
-        self.client.get(f"/{short_code}")
+    @task(1)
+    def redirect_link(self):
+        # Тут желательно подставлять реальные короткие ссылки, для теста возьмем заглушку
+        self.client.get("/abc123")
