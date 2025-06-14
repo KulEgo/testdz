beb1bbb3e897c8fee487cbf9b2c4299d61bad617
KulEgo <guitarmeeeeeeen@gmail.com>
2025-06-14 17:48:54 +0300

    Update locustfile.py

--- tests/locust/locustfile.py
+++ tests/locust/locustfile.py
@@ -1,6 +1,12 @@
-from locust import HttpUser, task
+from locust import HttpUser, task, between
 
 class ShortistUser(HttpUser):
+    wait_time = between(1, 3)
+
     @task
     def shorten(self):
-        self.client.post("/api/shorten", json={"target_url": "https://example.com"})
+        response = self.client.post("/api/shorten", json={"target_url": "https://example.com"})
+        if response.status_code == 200:
+            short_code = response.json().get("short_code")
+            if short_code:
+                self.client.get(f"/{short_code}", catch_response=True)
