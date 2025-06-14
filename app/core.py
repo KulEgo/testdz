1f926398609a99967dde7b6c0a1d29a37521aea1
KulEgo <guitarmeeeeeeen@gmail.com>
2025-06-14 19:35:45 +0300

    Update core.py

--- app/core.py
+++ app/core.py
@@ -1,7 +1,5 @@
-import hashlib
+import random
+import string
 
-def generate_short_code(url: str) -> str:
-    # Простая функция генерации кода — взять первые 6 символов md5
-    m = hashlib.md5()
-    m.update(url.encode('utf-8'))
-    return m.hexdigest()[:6]
+def generate_short_link(length=6):
+    return ''.join(random.choices(string.ascii_letters + string.digits, k=length))
